b6a9e096c97ee8c4397fc49a0614c255cedf91eb
Hongke Qin <hongke@amazon.com>
2014-04-22 18:04:25 -0700

    Done EditDistance

--- src/hongke/interview/leetcode/questions/EditDistance.java
+++ src/hongke/interview/leetcode/questions/EditDistance.java
@@ -1,16 +1,17 @@
 package hongke.interview.leetcode.questions;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 
 /*
-  “” a b c d e f
-“” 0 1 2 3 4 5 6
-c  1 1
-a  2 1
-x  3
+  "" a b c d e f
+"" 0 1 2 3 4 5 6
+c  1 1 ..
+a  2 1 ..
+x  3 ..
 z  4
 f  5
 g  6
@@ -19,8 +20,9 @@ g  6
 /**
  * Created by hongke on 4/20/14.
  */
+@SuppressWarnings("unused")
 public class EditDistance {
-    Map<String, Integer> cache;
+    Map<String, Integer> cache = new HashMap<String, Integer>();
 
     public int minDistance(String word1, String word2) {
         long start = System.nanoTime();
@@ -34,17 +36,58 @@ public class EditDistance {
                 return word1.length();
             }
 
-            cache = new HashMap<String, Integer>();
-
-            result = findDistance(word1, word2);
+            result = iterative(word1, word2);
+//            result = recursive(word1, word2);
             return result;
         } finally {
             long end = System.nanoTime();
-            System.out.println("Result: " + result + ", Time: " + (end - start) + " ns , Cache size: " + cache.size() +  ", W1: " + word1 + "， W2: " + word2);
+            System.out.println("Result: " + result + ", Time: " + (end - start) + " ns , Cache size: " + cache.size() +  ", W1: " + word1 + ", W2: " + word2);
         }
     }
+    
+    private int recursive(String word1, String word2) {
+        return findDistanceRecursively(word1, word2);
+    }
+    
+    private int iterative(String word1, String word2) {
+    	return findDistanceIteratively(word1, word2);
+    }
+    
+    private int findDistanceIteratively(String s, String t) {
+    	Integer[][] cache = new Integer[t.length() + 1][s.length() + 1];
+    	for (int i = 0; i <= t.length(); i++ ) {
+    		cache[i][0] = i;
+    	}
+    	for (int i = 0; i <= s.length(); i++ ) {
+    		cache[0][i] = i;
+    	}
+    	
+    	for (int i = 1; i <= t.length(); i++) {
+        	for (int j = 1; j <= s.length(); j++) {
+        		if (t.charAt(i - 1) == s.charAt(j - 1)) {
+        			cache[i][j] = cache[i - 1][j - 1];
+        		} else {
+        			cache[i][j] = min(cache[i - 1][j], cache[i][j - 1], cache[i - 1][j - 1]) + 1;
+        		}
+        	}
+    	}
+    	
+    	return cache[t.length()][s.length()];
+    }
+    
+    private int min(Integer ...numbers) {
+    	int min = Integer.MAX_VALUE;
+    	for (Integer i : numbers) {
+    		if (i < min) {
+    			min = i;
+    		}
+    	}
+    	return min;
+    }
+    
 
-    private int findDistance(String s, String t) {
+
+    private int findDistanceRecursively(String s, String t) {
         if (s.length() == 0) {
             return t.length();
         } else if (t.length() == 0) {
@@ -66,13 +109,13 @@ public class EditDistance {
         int value = 0;
         if (Character.compare(s.charAt(0), t.charAt(0)) == 0) {
             // no edit
-            value = findDistance(s.substring(1), t.substring(1));
+            value = findDistanceRecursively(s.substring(1), t.substring(1));
         } else {
             // edit
             String d = delete(s);
             String i = insert(t.charAt(0), s);
             String r = replace(t.charAt(0), s);
-            Integer[] values = new Integer[] {findDistance(r.intern(), t) /*replace*/, findDistance(i.intern(), t)/*insert*/, findDistance(d.intern(), t) /*delete*/};
+            Integer[] values = new Integer[] {findDistanceRecursively(r.intern(), t) /*replace*/, findDistanceRecursively(i.intern(), t)/*insert*/, findDistanceRecursively(d.intern(), t) /*delete*/};
             Arrays.sort(values);
             value = 1 + values[0];
         }
